99347d4ad7a637e672ceb1930bfbc477b2c58924
keisukefukuchi <kekke53530708@gmail.com>
2024-01-07 04:19:30 +0900

    fix

--- demo/src/main/java/com/example/demo/controller/CalendarController.java
+++ demo/src/main/java/com/example/demo/controller/CalendarController.java
@@ -31,45 +31,20 @@ public class CalendarController {
         this.paymentTypeService = paymentTypeService;
     }
 
-    @GetMapping("/calendar")
+    @GetMapping("/")
     public String showCalendar(Model model) {
-        model.addAttribute("searchForm", new SearchForm());
-        return populateCalendar(model, YearMonth.now());
-    }
-
-    @PostMapping("/calendar")
-    public String searchCalendar(SearchForm searchForm, Model model) {
-        YearMonth yearMonth = YearMonth.parse(searchForm.getYearMonth().toString());
-        return populateCalendar(model, yearMonth);
+        return "calendar";
     }
 
     @GetMapping("/expense")
     public String handleDateClick(@RequestParam String selectedDate, Model model) {
         // 日付が正常に受信されたときの処理
         System.out.println("受け取ったデータ: " + selectedDate);
-
         // ページに表示するデータを設定
         model.addAttribute("selectedDate", selectedDate);
         model.addAttribute("categories", categoryService.getAllCategories());
         model.addAttribute("paymentTypes", paymentTypeService.getAllPaymentTypes());
-
         return "expense";
-    }
-
-    private String populateCalendar(Model model, YearMonth yearMonth) {
-        // カレンダーの日付をリストに追加
-        List<String> dates = new ArrayList<>();
-        LocalDate firstDay = yearMonth.atDay(1);
-        LocalDate lastDay = yearMonth.atEndOfMonth();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-        while (!firstDay.isAfter(lastDay)) {
-            dates.add(firstDay.format(formatter));
-            firstDay = firstDay.plusDays(1);
-        }
-        model.addAttribute("yearMonth", yearMonth);
-        model.addAttribute("dates", dates);
-        return "calendar";
     }
 }
 
